c093a217a4b94b76d078c80270d66c3ff31eeda0
Shady <107540007+shadyo6@users.noreply.github.com>
2022-09-21 14:02:03 +0530

    Update 03_InsertionSort.c

--- DAA/03_InsertionSort.c
+++ DAA/03_InsertionSort.c
@@ -1,35 +1,57 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
-int main(){
-	int i,j,temp,n,A[10000];
-	time_t start,end;
-	double cpu_exe_t;
-	printf("enter number of elements: ");
+#define range 500
+#define max 500
+int A[max];
+
+void insertionSort(int);
+
+int main()
+{
+	srand(time(0))
+	int i,j,n;
+	clock_t s,e;
+	double cpu_exe_time;
+	
+	printf("Enter number of elements: ");
 	scanf("%d",&n);
+	
 	for(i=0 ; i<n ; i++)
-		A[i] = rand()%100;
+		A[i] = rand()%range;
+	
 	printf("\n the array elements are: \n");
 	for(i=0 ; i<n ; i++)
 		printf("%d  ",A[i]);
-	start = clock();
-	for(int k=0 ; k<100000 ; k++){
-		for(i=1 ; i<n ; i++){
-			temp = A[i];
-			j = i-1;
-			while(A[j]>=temp && j>=0){
-				A[j+1] = A[j];
-				j--;
-			}
-			A[j+1] = temp;
-		}
-	}
-	end = clock();
-	cpu_exe_t = (double)(end-start)/CLOCKS_PER_SEC;
-	printf("\norder of sorted elements: \n");
+	
+	s = clock();
+	for(int k=0 ; k<100000 ; k++)
+		insertionSort(n);
+	e = clock();
+	
+	//cpu_exe_time = (double)(e-s)/CLK_TCK;
+	cpu_exe_time = (double)(e-s)/CLOCKS_PER_SEC;
+	printf("\nOrder of sorted elements: \n");
 	for(i=0 ; i<n ; i++)
 		printf("%d  ",A[i]);
-	printf("\nCPU execution time:  %.2f \n",cpu_exe_t);
+	
+	printf("\nCPU execution time:  %lf \n",cpu_exe_t);
 	return 0;
+}
+
+void insertionSort(int n)
+{
+	int min;
+	for(i=1 ; i<n ; i++)
+	{
+		min = A[i];
+		j = i-1;
+		while(A[j]>=min && j>=0)
+		{
+			A[j+1] = A[j];
+			j--;
+		}
+		A[j+1] = min;
+	}
 }

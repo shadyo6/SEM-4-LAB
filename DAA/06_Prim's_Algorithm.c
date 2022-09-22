9e4c5dcf35559772ddff55e598c3353ced1320f0
Shady <107540007+shadyo6@users.noreply.github.com>
2022-09-23 00:04:56 +0530

    Update 06_Prim's_Algorithm.c

--- DAA/06_Prim's_Algorithm.c
+++ DAA/06_Prim's_Algorithm.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define infinity 999
 
-int a,b,u,v,n,i,j,ne=1;
+int u,v,n,i,j,ne=1;
+int min,mincost=0,cost[10][10],visited[10]={0};
 
-int visited[10]={0},min,mincost=0,cost[10][10];
-
-void main()
+int main(void)
 {
-    printf("\n enter the no. of nodes:  ");
-    scanf("\n %d",&n);
-    printf("\n enter the adjacency matrix: \n");
-
+    printf("\nEnter the number of nodes: ");
+    scanf("%d",&n);
+    
+    printf("\nEnter the adjacency matrix: \n");
     for(i=1;i<=n;i++)
         for(j=1;j<=n;j++)
-    {
-        scanf("%d",&cost[i][j]);
-        if(cost[i][j]==0)
-            cost[i][j]=999;
-    }
+        {
+            scanf("%d",&cost[i][j]);
+            if(cost[i][j]==0)
+                cost[i][j]=infinity;
+        }
 
 
     visited[1]=1;
-    printf("\n");
-
     while(ne<n)
     {
-        for(i=1,min=999;i<=n;i++)
-         for(j=1;j<=n;j++)
-
-           if(cost[i][j] < min)
-             if(visited[i]!=0)
-        {
-            min = cost[i][j];
-            a=u=i;
-            b=v=j++;
-        }
+        min=infinity;
+        for(i=1;i<=n;i++)
+            for(j=1;j<=n;j++)
+                if(cost[i][j] < min && visited[i]!=0)
+                {
+                    min = cost[i][j];
+                    u=i;
+                    v=j++;
+                }
 
         if(visited[u]==0 || visited[v]==0)
         {
-            printf("\n edge %d:(%d %d)  cost : %d",ne++, a,b,min);
+            printf("\nEdge %d:(%d %d) cost : %d",ne++,u,v,min);
 
             mincost+=min;
-            visited[b]=1;
+            visited[v]=1;
         }
 
-        cost[a][b]=cost[b][a]=999;
+        cost[u][v]=cost[v][u]=infinity;
     }
 
-    printf("\n minimum cost: %d\n" ,mincost);
-
+    printf("\nMinimum cost: %d\n" ,mincost);
+    return 0;
 }
